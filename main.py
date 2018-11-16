b4f5430c411c545f0042a49bf01b6a19083e3467
JTL <jtl@teamclassified.ca>
2018-11-15 17:28:11 -0800

    Attempt to diff two images by subtracting the previous frame from the current captured frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-
+from skimage.measure import compare_ssim
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -16,9 +16,16 @@ while(True):
        pass
         
     img = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
+    try:
+        (score, diff) = compare_ssim(img, old_img, full=True, multichannel=True)
+    except NameError:
+        print "restart loop"
+        continue
+
+    diff = (diff * 255).astype("uint8")
 
     # Display the resulting frame
-    cv2.imshow('img',img)
+    cv2.imshow('diff', diff)
 
     # quit when 'q' is pressed on the image window
     if cv2.waitKey(1) & 0xFF == ord('q'):
