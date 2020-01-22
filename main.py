4fd5829acdd697feaf7b3dd18ce9053ab99bff2d
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-01-22 08:55:13 -0800

    Separate render from show / save
    add two flags for showing or saving
    non functioning saving

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
 
     parser = argparse.ArgumentParser(description="Compare frames from a video or capture device")
     group = parser.add_mutually_exclusive_group()
+    group.add_argument("--display", "-d", action='store_true')
+    group.add_argument("--output", "-o", type=str, help="Output file")
+    group = parser.add_mutually_exclusive_group()
     group.add_argument("--cap", type=int, help="Index value of cv2.VideoCapture device")
     group.add_argument("--file", type=str, help="Path to AVI file to use instead of a video device")
     args = parser.parse_args()
@@ -16,7 +19,11 @@ def main():
     source = args.cap if args.cap else args.file
 
     video = VideoDiff(source)
-    video.render()
+
+    if args.display is True:
+        video.show()
+    if args.output:
+        video.save(args.output)
 
 
 if __name__ == '__main__':
--- util/video.py
+++ util/video.py
@@ -1,4 +1,6 @@
 import cv2
+import time
+start_time = time.time()
 
 
 class VideoDiff():
@@ -12,6 +14,7 @@ class VideoDiff():
         self.cap = cv2.VideoCapture(source)
         self.state = 'b'
 
+
     def __del__(self):
         # When everything done, release the capture
         cv2.destroyAllWindows()
@@ -22,28 +25,11 @@ class VideoDiff():
         if cv2.waitKey(1) and 0xFF == ord(key):
             return key
 
-    def render(self):
-        prevcolor = False
-        while self.cap.isOpened():
-            # Capture frame-by-frame
-            ret, frame = self.cap.read()
-
-            # Save the previous frame
-            if prevcolor is not False:
-                prevcolor = color
-
-
-            # Need to validate if this is the proper way to extract subpixel values
-            colorindex = self.colortoindex[self.state]
-            color = frame[:, :, colorindex]
-
-            # Display the resulting frame
-            if prevcolor is not False:
-                windowname = 'diff {}'.format(self.state)
-                image = color - prevcolor
-                cv2.imshow(windowname, image)
-            else:
-                prevcolor = color
+    def show(self):
+        windowname = 'diff {}'.format(self.state)
+        for vimage in self.__render():
+            cv2.imshow(windowname, vimage)
+            print("--- %s seconds ---" % (time.time() - start_time))
 
             # quit when 'q' is pressed on the image window
             if self.getKeyBind('q'):
@@ -54,3 +40,39 @@ class VideoDiff():
                 self.state = 'g'
             if self.getKeyBind('b'):
                 self.state = 'b'
+
+    def save(self, path):
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        out = cv2.VideoWriter(path, fourcc, 20.0, (height, width))
+
+        for vimage in self.__render():
+            out.write(vimage)
+            print("--- %s seconds ---" % (time.time() - start_time))
+        print("done")
+        out.release()
+
+    def __render(self):
+        prevcolor = False
+        while self.cap.isOpened():
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+            if ret is True:
+                # Save the previous frame
+                if prevcolor is not False:
+                    prevcolor = color
+
+                # Grab color index set by self state and retrieve from frame
+                colorindex = self.colortoindex[self.state]
+                color = frame[:, :, colorindex]
+
+                # First run, save color as prevcolor and skip
+                # Then compare the two
+                if prevcolor is not False:
+                    image = color - prevcolor
+                else:
+                    prevcolor = color
+                    continue
+
+                yield image
