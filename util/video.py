73cf70079ed986485655bd3926f348db7a8daf52
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-01-25 23:03:34 -0800

    Split Dithering class from VideoDiff

--- util/video.py
+++ util/video.py
@@ -3,54 +3,19 @@ import numpy as np
 
 
 class VideoDiff:
-    def __init__(self, source, fill_value=0, dither_method="diff", state="g"):
-        self.colortoindex = {
-            "b": 0,
-            "g": 1,
-            "r": 2,
-        }
-
-        self.fill_value = fill_value
+    def __init__(self, source):
         self.cap = cv2.VideoCapture(source)
-        self.state = state
-        self.dither_method = dither_method
 
     def __del__(self):
         # When everything done, release the capture
         cv2.destroyAllWindows()
         self.cap.release()
 
-    @staticmethod
-    def getKeyBind(key):
-        if cv2.waitKey(1) == ord(key):
-            return key
-
     def show(self):
         try:
-            for vimage in self.__render():
-                # if self.dither_method == "diff":
-                #     windowname = 'Diff {}'.format(self.state.upper())
-                # elif self.dither_method == "mask":
-                #     windowname = "Masked"
+            for vimage in self._render():
                 windowname = "Dither"
                 cv2.imshow(windowname, vimage)
-
-                # quit when 'q' is pressed on the image window
-                if self.getKeyBind('q'):
-                    print("q: Quit program")
-                    break
-                if self.getKeyBind('r') and self.state != 'r':
-                    print("r: Switching to red channel")
-                    self.state = 'r'
-                if self.getKeyBind('g') and self.state != 'g':
-                    print("g: Switching to green channel")
-                    self.state = 'g'
-                if self.getKeyBind('b') and self.state != 'b':
-                    print("b: Switching to blue channel")
-                    self.state = 'b'
-                if self.getKeyBind('m') and self.state != 'm':
-                    print("m: Switching to masking method")
-                    self.state = 'm'
 
         except KeyboardInterrupt:
             print("\nExiting")
@@ -61,35 +26,79 @@ class VideoDiff:
         fps = self.cap.get(cv2.CAP_PROP_FPS)
         width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        out = cv2.VideoWriter(path, fourcc, fps, (width, height))
 
-        for vimage in self.__render():
+        out = cv2.VideoWriter(path, fourcc, fps, (width, height))
+        for vimage in self._render():
             out.write(vimage)
         out.release()
 
-    def __render(self):
+    def _render(self):
+        raise AttributeError('Should be defined in subclass')
+
+
+class Dithering(VideoDiff):
+    def __init__(self, source, fill_value=0, dither_method="diff", state="g"):
+        super(Dithering, self).__init__(source=source)
+        self.fill_value = fill_value
+        self.dither_method = dither_method
+        self.state = state
+
+        self.colortoindex = {
+            "b": 0,
+            "g": 1,
+            "r": 2,
+        }
+
+    @staticmethod
+    def __subtraction(fframe, fprevframe, colortoindex, state=None):
+        # Zero out all color indexes not specified
+        # instead of extracting just the index
+        colorindex = colortoindex[state]
+        for index in colortoindex.values():
+            if index != colorindex:
+                fframe[:, :, index] = 0
+        frame_difference = fframe - fprevframe
+        return frame_difference
+
+    @staticmethod
+    def __mask(fframe, fprevframe, fill_value):
+        # Mask frame over old frame
+        # If element is different, change value to fill_value
+        imagemask = np.ma.masked_where(fframe != fprevframe, fframe)
+        imagemask.set_fill_value(fill_value)
+        masked_frame = imagemask.filled()
+        return masked_frame
+
+    def __frame_input(self):
+        def getkeybind(key):
+            if cv2.waitKey(1) == ord(key):
+                return key
+        # quit when 'q' is pressed on the image window
+        if getkeybind('q'):
+            print("q: Quit program")
+            exit(0)
+        elif getkeybind('r') and self.state != 'r':
+            print("r: Switching to red channel")
+            self.state = 'r'
+        elif getkeybind('g') and self.state != 'g':
+            print("g: Switching to green channel")
+            self.state = 'g'
+        elif getkeybind('b') and self.state != 'b':
+            print("b: Switching to blue channel")
+            self.state = 'b'
+        elif getkeybind('m') and self.state != 'm':
+            print("m: Switching to masking method")
+            self.state = 'm'
+
+    def _render(self):
         prevframe = False
         color = None
 
-        def subtraction(fframe, fprevframe, colortoindex):
-            # Zero out all color indexes not specified
-            # instead of extracting just the index
-            colorindex = colortoindex[self.state]
-            for index in colortoindex.values():
-                if index != colorindex:
-                    frame[:, :, index] = 0
-            frame_difference = fframe - fprevframe
-            return frame_difference
-
-        def mask(fframe, fprevframe, fill_value):
-            # Mask frame over old frame
-            # If element is different, change value to fill_value
-            imagemask = np.ma.masked_where(fframe != fprevframe, fframe)
-            imagemask.set_fill_value(fill_value)
-            masked_frame = imagemask.filled()
-            return masked_frame
+        self.__frame_input()
 
         while self.cap.isOpened():
+            self.__frame_input()
+
             # Capture frame-by-frame
             ret, frame = self.cap.read()
             if ret is True:
@@ -99,13 +108,13 @@ class VideoDiff:
                 color = frame
 
                 # First run, save color as prevframe and skip
-                # create mask of image of all changed values
+                # create __mask of image of all changed values
                 # Fill changed values to 255
                 if prevframe is not False:
                     if self.state in self.colortoindex.keys():
-                        image = subtraction(color, prevframe, self.colortoindex)
+                        image = self.__subtraction(color, prevframe, self.colortoindex, state=self.state)
                     elif self.state == 'm':
-                        image = mask(color, prevframe, self.fill_value)
+                        image = self.__mask(color, prevframe, self.fill_value)
                 else:
                     prevframe = color
                     continue
