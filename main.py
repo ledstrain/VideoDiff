102473a760cc1f6aabacaa59f6776d370c35ca11
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-01-25 20:10:56 -0800

    Merge VideoDiff branch to master

---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from util.video import VideoDiff
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Compare frames from a video or capture device")
+
+    parser.add_argument(
+            "--fill-value",
+            type=int,
+            help="Used with mask method, fill value for detected image changes.")
+    parser.add_argument(
+            "--dither-method",
+            "-x",
+            default="g",
+            choices=("r", "g", "b", "m"),
+            help="Dither detection method")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+            "--display",
+            "-d",
+            action='store_true')
+    group.add_argument(
+            "--output",
+            "-o",
+            type=str,
+            help="Output file, must be .avi format")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+            "--cap",
+            type=int,
+            help="Index value of cv2.VideoCapture device")
+    group.add_argument(
+            "--file",
+            type=str,
+            help="Path to AVI file to use instead of a video device")
+
+    args = parser.parse_args()
+
+    source = args.cap if args.cap is not None else args.file
+
+    video = VideoDiff(
+        source,
+        fill_value=args.fill_value,
+        state=args.dither_method,
+    )
+
+    if args.display is True:
+        video.show()
+    if args.output:
+        video.save(args.output)
+
+
+if __name__ == '__main__':
+    main()
---
+++ util/video.py
@@ -0,0 +1,116 @@
+import cv2
+import numpy as np
+
+
+class VideoDiff:
+    def __init__(self, source, fill_value=0, dither_method="diff", state="g"):
+        self.colortoindex = {
+            "b": 0,
+            "g": 1,
+            "r": 2,
+        }
+
+        self.fill_value = fill_value
+        self.cap = cv2.VideoCapture(source)
+        self.state = state
+        self.dither_method = dither_method
+
+    def __del__(self):
+        # When everything done, release the capture
+        cv2.destroyAllWindows()
+        self.cap.release()
+
+    @staticmethod
+    def getKeyBind(key):
+        if cv2.waitKey(1) == ord(key):
+            return key
+
+    def show(self):
+        try:
+            for vimage in self.__render():
+                # if self.dither_method == "diff":
+                #     windowname = 'Diff {}'.format(self.state.upper())
+                # elif self.dither_method == "mask":
+                #     windowname = "Masked"
+                windowname = "Dither"
+                cv2.imshow(windowname, vimage)
+
+                # quit when 'q' is pressed on the image window
+                if self.getKeyBind('q'):
+                    print("q: Quit program")
+                    break
+                if self.getKeyBind('r') and self.state != 'r':
+                    print("r: Switching to red channel")
+                    self.state = 'r'
+                if self.getKeyBind('g') and self.state != 'g':
+                    print("g: Switching to green channel")
+                    self.state = 'g'
+                if self.getKeyBind('b') and self.state != 'b':
+                    print("b: Switching to blue channel")
+                    self.state = 'b'
+                if self.getKeyBind('m') and self.state != 'm':
+                    print("m: Switching to masking method")
+                    self.state = 'm'
+
+        except KeyboardInterrupt:
+            print("\nExiting")
+            exit(0)
+
+    def save(self, path):
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        fps = self.cap.get(cv2.CAP_PROP_FPS)
+        width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        out = cv2.VideoWriter(path, fourcc, fps, (width, height))
+
+        for vimage in self.__render():
+            out.write(vimage)
+        out.release()
+
+    def __render(self):
+        prevframe = False
+
+        def subtraction(fframe, fprevframe, colortoindex):
+            # Zero out all color indexes not specified
+            # instead of extracting just the index
+            colorindex = colortoindex[self.state]
+            for index in colortoindex.values():
+                if index != colorindex:
+                    frame[:, :, index] = 0
+            frame_difference = fframe - fprevframe
+            return frame_difference
+
+        def mask(fframe, fprevframe, fill_value):
+            # Mask frame over old frame
+            # If element is different, change value to fill_value
+            imagemask = np.ma.masked_where(fframe != fprevframe, fframe)
+            imagemask.set_fill_value(fill_value)
+            masked_frame = imagemask.filled()
+            return masked_frame
+
+        while self.cap.isOpened():
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+            if ret is True:
+                # Save the previous frame
+                if prevframe is not False:
+                    prevframe = color
+                color = frame
+
+                # First run, save color as prevframe and skip
+                # create mask of image of all changed values
+                # Fill changed values to 255
+                if prevframe is not False:
+                    if self.state in self.colortoindex.keys():
+                        image = subtraction(color, prevframe, self.colortoindex)
+                    elif self.state == 'm':
+                        image = mask(color, prevframe, self.fill_value)
+                else:
+                    prevframe = color
+                    continue
+
+                yield image
+
+            else:
+                # Once video has no more frames
+                break
