d66d45bea8ad197780d457914b76be3ae970fefd
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-04-08 11:57:25 -0700

    default to display
    formatting for other options

--- videodiff.py
+++ videodiff.py
@@ -13,32 +13,39 @@ def main():
     parser.add_argument(
             "--fill-value",
             type=int,
-            help="Used with mask method, fill value for detected image changes.")
+            help="Used with mask method, fill value for detected image changes.",
+            )
     parser.add_argument(
             "--dither-method",
             "-x",
             default="g",
             choices=("r", "g", "b", "m", "n"),
-            help="Dither detection method")
+            help="Dither detection method",
+            )
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
             "--display",
             "-d",
-            action='store_true')
+            action='store_true',
+            default=True,
+            )
     group.add_argument(
             "--output",
             "-o",
             type=str,
-            help="Output file, must be .avi format")
+            help="Output file, must be .avi format",
+            )
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
             "--cap",
             type=int,
-            help="Index value of cv2.VideoCapture device")
+            help="Index value of cv2.VideoCapture device",
+            )
     group.add_argument(
             "--file",
             type=str,
-            help="Path to AVI file to use instead of a video device")
+            help="Path to AVI file to use instead of a video device",
+            )
 
     if len(argv) < 2:
         parser.print_usage()
