4883e16d3149414ab401b6d2dc65777b5f1fe711
JTL <jtl@teamclassified.ca>
2023-03-17 23:34:18 -0700

    randomnoise: Add option to overlay file numbering in output images

--- randomnoise.py
+++ randomnoise.py
@@ -14,12 +14,19 @@ class FrameGenerator:
     def generate_random_frame(self, height, width, frameinfo=None):
         frame = self.rng.integers(256, size=(height, width, 3), dtype=np.uint8)
         if frameinfo:
-            cv2.putText(frame, "{frame}/{totalframes}".format(frame=frameinfo['frame'], totalframes=frameinfo['totalframes']), (0, 20), cv2.FONT_HERSHEY_COMPLEX, 0.8*height, [255, 255, 255], 1, cv2.LINE_AA);
+            frametext = "{frame}/{totalframes}".format(frame=frameinfo['frame'], totalframes=frameinfo['totalframes'])
+            fontScale = min(width, height) * 0.0017
+            yPos = int(height / 20)
+            cv2.putText(frame, frametext, (0, yPos), cv2.FONT_HERSHEY_SIMPLEX, fontScale, [255, 255, 255], 3, cv2.FILLED);
+
         return frame
 
-    def generate_frames(self, output=None):
+    def generate_frames(self, output=None, frameinfo=None):
         for i in range(0, self.sequence):
-            self.frames.append(self.generate_random_frame(self.height, self.width, {'frame': i, 'totalframes': self.sequence}))
+            if frameinfo:
+                self.frames.append(self.generate_random_frame(self.height, self.width, {'frame': i, 'totalframes': self.sequence}))
+            else:
+                self.frames.append(self.generate_random_frame(self.height, self.width))
             if output:
                 output_file = "{output_path}/{frame}.tiff".format(output_path=output, frame=i)
                 if cv2.haveImageWriter(output_file):
@@ -76,11 +83,16 @@ def main():
             default="output",
             help="Mode of operation",
     )
-
+    parser.add_argument(
+            "--frameinfo",
+            action="store_true",
+            default=False,
+            help="Whether to overlay frame number markings on output",
+    )
 
     args = parser.parse_args()
     fg = FrameGenerator(args.frames, args.width, args.height, args.seed)
-    fg.generate_frames(args.output)
+    fg.generate_frames(args.output, args.frameinfo)
     if args.mode == 'display':
         fg.display_as_video()
 
