ledstrain/VideoDiff

commit 1f8e228f3054dde020009e8845f9034daf9a5a6d

Properly handle directory creation for --output

videodiff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from os import path
+from os import path, makedirs
 from sys import argv, exit
 from util.video import SimpleDither
 from util.image import ImageDiff
@@ -75,12 +75,20 @@ def main():
             fill_value=args.fill_value,
             state=args.dither_method,
         )
+    if args.output is not None: 
+        if not path.exists(args.output):
+                try:
+                    makedirs(args.output)
+                except Exception as e:
+                    print("Unable to create directory")
+                    print(e)
+                    exit(1)
 
         if path.exists("{output}/2.tiff".format(output=args.output)):
             print("Refusing to overwrite existing capture output")
             exit(1)
 
-        video.process(display=args.display, output_path=args.output)
+    video.process(display=args.display, output_path=args.output)
 
     if args.mode == 'image':
         if len(args.file) != 2:

commit d66a6349a1d2be056c4403ee0a85b890b7fa4760

Fix `--mode image` (broken by `1f8e228f`)
To reproduce `python videodiff.py --mode image --file a.tiff c.tiff --display`

videodiff.py
@@ -63,18 +63,6 @@ def main():
 
     filelen = len(args.file)
     mode = args.mode
-
-    if args.mode == "dithering":
-        if len(args.file) > 1:
-            print("Only one file is allowed")
-            exit(1)
-        file = args.file[0]
-        source = args.cap if args.cap is not None else file
-        video = SimpleDither(
-            source,
-            fill_value=args.fill_value,
-            state=args.dither_method,
-        )
     if args.output is not None: 
         if not path.exists(args.output):
                 try:
@@ -88,7 +76,19 @@ def main():
             print("Refusing to overwrite existing capture output")
             exit(1)
 
-    video.process(display=args.display, output_path=args.output)
+    if args.mode == "dithering":
+        if len(args.file) > 1:
+            print("Only one file is allowed")
+            exit(1)
+        file = args.file[0]
+        source = args.cap if args.cap is not None else file
+        video = SimpleDither(
+            source,
+            fill_value=args.fill_value,
+            state=args.dither_method,
+            framebyframe=args.pause,
+        )
+        video.process(display=args.display, output_path=args.output)
 
     if args.mode == 'image':
         if len(args.file) != 2: