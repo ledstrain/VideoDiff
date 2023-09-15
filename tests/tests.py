26352bd9bf5d1fd705c44e27fad5351541ccee8f
JTL <jtl@teamclassified.ca>
2023-09-15 02:36:45 -0700

    Add some unit tests

---
+++ tests/tests.py
@@ -0,0 +1,41 @@
+import pytest
+import numpy as np
+import cv2
+from util.video import SimpleDither
+
+class TestVideoDiff:
+    def test_red(self):
+        tpframes = []
+        videoframes = []
+        for i in range(2, 6):
+            tpframes.append(cv2.imread("testpatterns/r/{i}.tiff".format(i=i)))
+
+        video = SimpleDither('testpatterns/testpattern.mkv', fill_value=255, state='r', framebyframe=False)
+        for frame in video._render(video.cap):
+            videoframes.append(frame)
+
+        assert np.array_equal(tpframes, videoframes)
+
+    def test_green(self):
+        tpframes = []
+        videoframes = []
+        for i in range(2, 6):
+            tpframes.append(cv2.imread("testpatterns/g/{i}.tiff".format(i=i)))
+
+        video = SimpleDither('testpatterns/testpattern.mkv', fill_value=255, state='g', framebyframe=False)
+        for frame in video._render(video.cap):
+            videoframes.append(frame)
+ 
+        assert np.array_equal(tpframes, videoframes)
+    
+    def test_blue(self):
+        tpframes = []
+        videoframes = []
+        for i in range(2, 6):
+            tpframes.append(cv2.imread("testpatterns/b/{i}.tiff".format(i=i)))
+
+        video = SimpleDither('testpatterns/testpattern.mkv', fill_value=255, state='b', framebyframe=False)
+        for frame in video._render(video.cap):
+            videoframes.append(frame)
+ 
+        assert np.array_equal(tpframes, videoframes)
